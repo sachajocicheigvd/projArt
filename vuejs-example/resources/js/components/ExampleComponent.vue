9fbe069762c728b072ed10d877bb1bedd9b29b49
Jocic Sacha <jocicsacha@mbp-de-jocic.einet.ad.eivd.ch>
2023-05-24 13:43:43 +0200

    test

---
+++ vuejs-example/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{ test.x }}
+    {{ test.y }}
+    
+</template>
+
+<script setup >
+  
+  const test =  defineProps(['x','y']);
+
+  console.log(test);
+
+</script>
